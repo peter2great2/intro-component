e25380d4d630c05cdd944fe51521fad0de2fc98a
peter2great2 <peter2great2@gmail.com>
2023-05-01 04:45:38 +0100

    added the error messages to the javascript file

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let firstName = document.getElementById("first-name");
 let firstNameError = document.getElementById("first-name-error");
 let lastName = document.getElementById("last-name");
 let lastNameError = document.getElementById("last-name-error");
+const email = document.getElementById("email");
+const emailError = document.getElementById("email-error");
 
 const btnClicked = () => {
   if (firstName.value === "") {
@@ -9,5 +11,8 @@ const btnClicked = () => {
   }
   if (lastName.value === "") {
     lastNameError.style.display = "inline";
+  }
+  if (email.value === "") {
+    emailError.style.display = "inline";
   }
 };
